2180dbdaf5455fb4e69454906d76b8861f65f707
kaio062 <kairobarbosa2007@gmail.com>
2025-11-09 23:33:20 -0300

    Update insert.sql

--- Projeto-DW-academia/insert.sql
+++ Projeto-DW-academia/insert.sql
@@ -80,43 +80,45 @@ INSERT INTO `academia`.`usuario` (`nome`, `email`, `senha`, `idade`) VALUES
 
 -- TABELA: usuario_treino (união dos pares, sem duplicações idênticas)
 INSERT INTO usuario_treino (usuario_id_usuario, treino_id_treino) VALUES
-(1, 1),
-(1, 3),
-(2, 2),
-(2, 5),
-(3, 3),
-(3, 4),
-(4, 1),
-(4, 4),
-(5, 5),
-(5, 6),
-(6, 3),
-(6, 6),
-(7, 7),
-(8, 2),
-(8, 8),
-(9, 9),
-(10, 10);
+(1, 5),
+(1, 2),
+(2, 4),
+(2, 9),
+(3, 1),
+(3, 8),
+(4, 3),
+(4, 10),
+(5, 7),
+(5, 1),
+(6, 2),
+(6, 9),
+(7, 4),
+(8, 6),
+(8, 3),
+(9, 5),
+(10, 8);
+
 
 -- TABELA: exercicio_treino (união dos pares, sem duplicações idênticas)
 INSERT INTO exercicio_treino (exercicio_id, treino_id) VALUES
-(1, 1),
-(2, 1),
 (3, 1),
-(6, 1),
-(1, 2), 
+(5, 1),
+(9, 1),
+(1, 1),
+(2, 2),
+(6, 2),
+(8, 2),
 (3, 2),
-(4, 2),
-(5, 2),
-(2, 3),
+(1, 3),
 (4, 3),
-(6, 3),
-(8, 3),
-(9, 4),
+(5, 3),
+(9, 3),
 (7, 4),
-(8, 4),
-(9, 5),
-(10, 5);
+(10, 4),
+(2, 4),
+(6, 5),
+(8, 5);
+
 
 -- TABELA: historico_treino (todos os registros)
 INSERT INTO historico_treino (usuario_id, treino_id, data_treino, duracao_minutos, observacoes) VALUES
